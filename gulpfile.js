7191453a47c36dfa6df2da5aee8afa94e4021c52
alicegherbison <alicegherbison@gmail.com>
2019-03-12 13:20:37 +0000

    Add uglify and gulp-if plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 var deploy = require('gulp-gh-pages');
 
 gulp.task('useref', function(){
   return gulp.src('src/*.html')
     .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
     .pipe(gulp.dest('dist'))
 });
 
