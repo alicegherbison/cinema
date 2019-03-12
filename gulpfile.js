0fbc5f6b40e895b502997ddc782821d79e135525
alicegherbison <alicegherbison@gmail.com>
2019-03-12 16:24:53 +0000

    Partially fix gulp async error

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,21 @@ var uglify = require('gulp-uglify');
 var runSequence = require('gulp4-run-sequence');
 var deploy = require('gulp-gh-pages');
 
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
+gulp.task('clean:dist', function(done) {
+  del.sync('dist');
+  done();
 });
 
 gulp.task('useref', function(){
   return gulp.src('src/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', function(callback) {
-  runSequence('clean:dist', 'useref', callback)
+gulp.task('build', function(done) {
+  runSequence('clean:dist', 'useref');
+  done();
 });
 
 gulp.task('deploy', function() {
