891301ded0077c44916dc577fb1b6998cf78fed8
alicegherbison <alicegherbison@gmail.com>
2019-03-12 16:29:23 +0000

    Add Babel; attempt second async error fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var del = require('del');
 var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
+var babel = require("gulp-babel");
 var uglify = require('gulp-uglify');
 var runSequence = require('gulp4-run-sequence');
 var deploy = require('gulp-gh-pages');
@@ -11,11 +12,13 @@ gulp.task('clean:dist', function(done) {
   done();
 });
 
-gulp.task('useref', function(){
-  return gulp.src('src/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'));
+gulp.task('useref', function(done){
+  gulp.src('src/*.html')
+  .pipe(useref())
+  .pipe(babel())
+  .pipe(gulpIf('*.js', uglify()))
+  .pipe(gulp.dest('dist'));
+  done();
 });
 
 gulp.task('build', function(done) {
@@ -25,5 +28,5 @@ gulp.task('build', function(done) {
 
 gulp.task('deploy', function() {
   return gulp.src("./dist/**/*")
-    .pipe(deploy())
+  .pipe(deploy())
 });
