c1002e3660d74c7d47842f4fdf8713c894f7594f
alicegherbison <alicegherbison@gmail.com>
2019-03-12 13:15:54 +0000

    Add useref plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var useref = require('gulp-useref');
 var deploy = require('gulp-gh-pages');
+
+gulp.task('useref', function(){
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+});
 
 gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
