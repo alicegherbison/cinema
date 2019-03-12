cc0daec9d90a2376dafe4e3440a7379d9f10779d
alicegherbison <alicegherbison@gmail.com>
2019-03-12 22:18:03 +0000

    Further attempt at gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
-var gulp = require('gulp');
-var del = require('del');
-var useref = require('gulp-useref');
-var gulpIf = require('gulp-if');
-var babel = require("gulp-babel");
-var uglify = require('gulp-uglify');
-var runSequence = require('gulp4-run-sequence');
-var deploy = require('gulp-gh-pages');
+const gulp = require('gulp');
+const del = require('del');
+const minify = require('gulp-minify');
+const concat = require('gulp-concat');
+const useref = require('gulp-useref');
+const gulpIf = require('gulp-if');
+const babel = require("gulp-babel");
+const uglify = require('gulp-uglify');
+const runSequence = require('gulp4-run-sequence');
+const deploy = require('gulp-gh-pages');
+
+function js() {
+  return gulp.src('src/js/*.js')
+  .pipe(concat('build.min.js'))
+  .pipe(minify())
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(gulp.dest('dist/js'));
+};
 
 gulp.task('clean:dist', function(done) {
   del.sync('dist');
@@ -50,3 +62,5 @@ gulp.task('deploy', function() {
   return gulp.src("./dist/**/*")
   .pipe(deploy())
 });
+
+exports.js = js;
